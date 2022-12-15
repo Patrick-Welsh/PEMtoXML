ee9b2cefe82362e8492e7073eda35979e16599d5
Patrick Welsh <patrick.welsh@n-able.com>
2022-12-15 07:27:06 +0000

    Add project files.

---
+++ PEMtoXML/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Xml;
+
+var privateKey = await File.ReadAllTextAsync("rsa.pem");
+var rsa = RSA.Create();
+rsa.ImportFromPem(privateKey.ToCharArray());
+var d = new XmlDocument();
+d.LoadXml(rsa.ToXmlString(true));
+d.Save("rsa.xml");
+
+
